0c8e7e49dd59b83b4fbe49562c13de9f3146a349
Mahesh <onlyminion9@gmail.com>
2024-02-09 14:45:38 +0530

    episode-o1

---
+++ App.js
@@ -0,0 +1,6 @@
+const heading = React.createElement("h1", { id: "heading", xyz: "abc" }, "Hello World From React!!");
+const parent = React.createElement("div",{id:"parent"},[React.createElement("div",{className:"child1 child"},React.createElement("h1",{},"Hello Mahesh")),
+React.createElement("div",{className:"child2 child"},React.createElement("h1",{},"Hello Swami"))]);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
+console.log(parent);
