afb3b6940823716863bd12857ad3183d981a5eeb
Mahesh <onlyminion9@gmail.com>
2024-02-10 18:57:56 +0530

    episode-01

--- App.js
+++ App.js
@@ -1,3 +1,5 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
 const heading = React.createElement("h1", { id: "heading", xyz: "abc" }, "Hello World From React!!");
 const parent = React.createElement("div",{id:"parent"},[React.createElement("div",{className:"child1 child"},React.createElement("h1",{},"Hello Mahesh")),
 React.createElement("div",{className:"child2 child"},React.createElement("h1",{},"Hello Swami"))]);
